e2283207b9df33c9631612e686bf7a48f075677d
Gor Sahakyan <saakyangor@gmail.com>
2023-06-05 22:08:39 +0400

    Go to git from local

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [val, setVal] = useState('')
+
+    function takeInput(e) {
+        setVal(e.target.value)
+    }
+
+    const [arr, setArr] = useState([])
+
+    function addObject(e) {
+        e.preventDefault()
+        const newObj = {
+            key: Date.now(),
+            text: val,
+            complete: false,
+        }
+
+        setArr([...arr, newObj])
+        setVal('')
+    }
+
+    function objDel(key) {
+        setArr(arr.filter(item => key !== item.key))
+    }
+
+    function objDone(key) {
+        setArr(arr.map((item) => item.key === key ? {...item, complete: !item.complete} : item));
+    }
+
+    console.log(arr)
+
+    return (
+        <div className="all">
+            <form action="" onSubmit={addObject}>
+                <input type="text" onChange={takeInput} value={val}/>
+                <button type="submit">Add</button>
+            </form>
+
+
+            <ul>
+                {
+                    arr.map(obj => (
+
+                        <li key={obj.key}>
+                            <input type="checkbox" onChange={() => objDone(obj.key)}/>
+                            {obj.text}
+                            <button onClick={() => objDel(obj.key)}>X</button>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+    );
 }
 
 export default App;
